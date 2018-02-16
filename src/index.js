29bb93c414a9fe26368ec5b7c45301a635431eb2
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-02-16 09:48:39 +0000

    shop function in need of code review

--- src/index.js
+++ src/index.js
@@ -82,22 +82,172 @@ exports.gibberish = gibberish;
 function calculator(){
   let total=0;
   function calculate(num1,num2,operator){
+    let innerTotal = 0
     switch(operator){
       case "+":
-        total = num1 + num2;
+        innerTotal = num1 + num2;
         break;
       case "-":
-        total = num1 - num2;
+        innerTotal = num1 - num2;
         break;
       case "*":
-        total = num1 * num2;
+        innerTotal = num1 * num2;
         break;
       case "/":
-        total = num1 / num2;
+        innerTotal = num1 / num2;
         break;
       default:
       console.log("no idea what you are doing")
     }
-
+    total += innerTotal
+    return total;
   }
+  return calculate;
+}
+
+exports.calculator = calculator;
+
+
+function randomAmount() {
+  return Math.floor((Math.random() * 20) + 1);
+}
+
+function randomPerson(passengerArrayLength) {
+  return Math.floor((Math.random() * passengerArrayLength) + 1);
+}
+
+
+function trainstation(){
+  let passengers = [{
+    name: 'Bob',
+    amount: 19,
+  },
+  {
+    name: 'Dave',
+    amount: 18
+  },
+  {
+    name: 'Ellen',
+    amount: 14
+  }];
+
+  return {
+    arrive : function(personObject) {
+      passengers.push(personObject);
+    },
+    getPeople : function(){
+      return passengers;
+    },
+    giveMoney : function(money,person){
+      passengers[person].amount += money;
+    },
+    trainArrives : function(){
+      let leavingPassengers = passengers.filter(passenger => passenger.amount >= 20);
+      return leavingPassengers;
+    }
+  };
+};
+
+exports.trainstation = trainstation;
+
+function dogHome(){
+  let dogHomes = {
+    battersea:[{
+      name: "pluto",
+      breed: "pointer",
+      colour: "yellow",
+      location: "battersea"
+    }],
+    margate:[{
+      name: "tracey",
+      breed: "mutt",
+      colour: "red",
+      location: "margate"
+    }],
+};
+  return{
+    houseDog : function(dogObj){
+    let dogLocation = dogObj.location;
+    if (dogHomes[dogLocation]){
+      dogHomes[dogLocation].push(dogObj);
+    }else{
+      dogHomes[dogLocation] = [dogObj];
+    }
+    return dogHomes;
+    },
+    getDogsByLocation: function(location){
+      return dogHomes[location];
+    }
+  };
+}
+
+exports.dogHome = dogHome;
+
+
+function shop() {
+  let revenue = 0;
+  let soldItemsArray = [];
+  let storage = {
+    cucumber: {
+      name: 'cucumber',
+      price: 5,
+      amount: 500,
+    },
+    gin : {
+      name: 'gin',
+      price: 20,
+      amount: 200,
+    },
+    tonic : {
+      name: 'tonic',
+      price: 2.5,
+      amount: 20,
+    }
+  };
+
+  return {
+    addStock : function(replenishArray) {
+      replenishArray.forEach(function(item) {
+        let name = item.name;
+        if (storage[name]) {
+          storage[name].amount += item.amount;
+          if (storage[name].price !== item.price) {
+            let newAveragePrice = (storage[name].price + item.price)/2;
+            storage[name].price = newAveragePrice;
+        }
+        } else {
+          storage[item.name] = item;
+        }
+        });
+      return storage;
+    },
+    sellStock : function(order) {
+      let soldItemsArray = [];
+      order.forEach(function(item) {
+        let name = item.name;
+        let soldItem = {};
+        if(storage[name]) {
+          let initStockAmount = storage[name].amount;
+          if (storage[name].amount > item.quantity) {
+            storage[name].amount -= item.quantity;
+            revenue += storage[name].price * item.quantity;
+            soldItem.name = storage[name].name;
+            soldItem.price = storage[name].price;
+            soldItem.quantity = item.quantity;
+          } else if (storage[name].amount < item.quantity) {
+            storage[name].amount = 0;
+            revenue += storage[name].price * initStockAmount;
+            soldItem.name = storage[name].name;
+            soldItem.price = storage[name].price;
+            soldItem.quantity = initStockAmount;
+          }
+        }
+        soldItemsArray.push(soldItem);
+      });
+      return soldItemsArray;
+    },
+    getRevenue : function() {
+      return revenue;
+    }
+  };
 }
--- test/index.test.js
+++ test/index.test.js
@@ -37,3 +37,138 @@ test('gibberish', function(){
   expect(result1).toEqual(expected1);
   expect(result2).toEqual(expected2);
 })
+
+test('calculator', function(){
+  const expected1 = 6;
+  const expected2 = 8;
+  const expected3 = 16;
+  const expected4 = 19;
+  const mainCalculator = functions.calculator();
+  const result1 = mainCalculator(4,2,"+");
+  const result2 = mainCalculator(4,2,"-");
+  const result3 = mainCalculator(4,2,"*");
+  const result4 = mainCalculator(9,3,"/");
+  expect(result1).toEqual(expected1);
+  expect(result2).toEqual(expected2);
+  expect(result3).toEqual(expected3);
+  expect(result4).toEqual(expected4);
+})
+
+let passengers = [{
+  name: 'Bob',
+  amount: 19,
+},
+{
+  name: 'Dave',
+  amount: 18
+},
+{
+  name: 'Ellen',
+  amount: 14
+}];
+
+let amendedPassengers = [{
+  name: 'Bob',
+  amount: 19,
+},
+{
+  name: 'Dave',
+  amount: 18
+},
+{
+  name: 'Ellen',
+  amount: 14
+},
+{
+  name: 'Oscar',
+  amount: 15
+}];
+
+test('trainstation', function(){
+  const expected1 = [{
+    name: 'Bob',
+    amount: 19,
+  },
+  {
+    name: 'Dave',
+    amount: 18
+  },
+  {
+    name: 'Ellen',
+    amount: 14
+  }];
+  const expected2 = [{
+      name: 'Bob',
+      amount: 19,
+    },
+    {
+      name: 'Dave',
+      amount: 18
+    },
+    {
+      name: 'Ellen',
+      amount: 14
+    },
+    {
+      name: 'Oscar',
+      amount: 15
+    }];
+  const { arrive, getPeople, pickRandomAmount, pickRandomPerson, giveMoney, trainArrives } = functions.trainstation();
+  const resultGetPeople1 = getPeople();
+  expect(expected1).toEqual(resultGetPeople1);
+  arrive({name: 'Oscar',amount: 15});
+  const resultGetPeople2 = getPeople();
+  expect(expected2).toEqual(resultGetPeople2);
+  const expectedGiveMoney = [{
+    name: 'Bob',
+    amount: 19,
+  },
+  {
+    name: 'Dave',
+    amount: 18
+  },
+  {
+    name: 'Ellen',
+    amount: 20
+  },
+  {
+    name: 'Oscar',
+    amount: 15
+  }];
+  giveMoney(6, 2);
+  const resultGetPeopleAfterGivingMoney = getPeople();
+  expect(expectedGiveMoney).toEqual(resultGetPeopleAfterGivingMoney);
+  const expectedLeaving =[{name: 'Ellen',amount: 20}];
+  const resultTrainLeaving = trainArrives();
+  expect(expectedLeaving).toEqual(resultTrainLeaving);
+})
+
+test('dogHome',function(){
+  const expected = { battersea:
+   [ { name: 'pluto',
+       breed: 'pointer',
+       colour: 'yellow',
+       location: 'battersea' },
+     { name: 'king',
+       breed: 'alsation',
+       colour: 'black',
+       location: 'battersea' } ],
+  margate:
+   [ { name: 'tracey',
+       breed: 'mutt',
+       colour: 'red',
+       location: 'margate' } ] };
+   const {houseDog,getDogsByLocation}=functions.dogHome();
+   const result = houseDog({name:"king", breed: "alsation", colour: "black", location: "battersea"});
+   expect(expected).toEqual(result);
+   const expected2 = [ { name: 'pluto',
+    breed: 'pointer',
+    colour: 'yellow',
+    location: 'battersea' },
+  { name: 'king',
+    breed: 'alsation',
+    colour: 'black',
+    location: 'battersea' } ];
+    const result2 = getDogsByLocation('battersea');
+    expect(expected2).toEqual(result2);
+})
